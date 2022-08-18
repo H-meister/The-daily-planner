ba06a261f354bce2747bc6f35fc443e2d4d37ae2
H-meister <105766365+H-meister@users.noreply.github.com>
2022-08-18 02:09:04 -0400

    adding pseudocode and adding classes described in css

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+//using moment to generate today's date
+var todaysdate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$('#currentDay').html(todaysdate);
+
+//trying to add save function
+    //1.target the save btn
+    //2.add event listener to do stuff
+    //3.said stuff will save 'textarea' to local storage
+    //4.retrieve it back somehow.
+
+
+//add a function that can track time 
+ //1. get current time prob using moment
+ //2. check time hours to see if i can add the colors neeeded
+ //3. remove them if current time is passed
+ 
+
+ //add retrieval from local storage at end.
