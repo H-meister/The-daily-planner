882b25b7da9559295d0528c61cf8c03d1e51ff81
H-meister <Hector1626hg@gmail.com>
2022-08-19 12:09:28 -0400

    trouble shooting making it work

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,66 @@ var todaysdate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $('#currentDay').html(todaysdate);
 
 //trying to add save function
-    //1.target the save btn
-    //2.add event listener to do stuff
-    //3.said stuff will save 'textarea' to local storage
-    //4.retrieve it back somehow.
+
+    $(".saveBtn").on("click", function() {
+        var notes = $(this).siblings('description').val();
+        var hour = $(this).parent().attr('id');
+
+        //save in local
+        JSON.stringify.localStorage.setItem(notes, hour);
+        // console.log('this is working');
+    })
+
+    var findHour = function() {
+        var currentHour = moment().hour();
+    
+
+    //check time in time blocks
+    // $('.time-block').each(function() {
+    //     var timeBlock = parseInt($(this).attr('#id').split('hour')[1]);
+    function timeBlock1() {
+       var check = parseInt($('hour5').split('hour')[1]);
+
+        if(check < currentHour) {
+            $('time-block').removeClass('future');
+            $('time-block').removeClass('present');
+            $('time-block').addClass('past');
+        }
+        else if (check === currentHour) {
+            $('time-block').removeClass('past');
+            $('time-block').removeClass('future');
+            $('time-block').addClass('present');
+        }
+        else {
+            $('time-block').removeClass('present');
+            $('time-block').removeClass('past');
+            $('time-block').addClass('future');
+        }
+        
+    
+    }
+    
+
+
+    function getLocal() {
+
+    
+    $('#hour5' .description).val(localStorage.getItem('hour5'));
+    $('#hour6' .description).val(localStorage.getItem('hour6'));   
+    $('#hour7' .description).val(localStorage.getItem('hour7'));
+    $('#hour8' .description).val(localStorage.getItem('hour8'));
+    $('#hour9' .description).val(localStorage.getItem('hour9'));
+    $('#hour10' .description).val(localStorage.getItem('hour10'));
+    }
+
+    findHour();
+    getLocal();
+};
+
+
+
+
+
 
 
 //add a function that can track time 
